e8f325acab6e53da225d8715762dbd6763266d09
YummYume <yummyume@users.noreply.github.com>
2024-02-18 16:20:46 +0100

    Format

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+  plugins: [sveltekit()],
   server: {
     watch: {
       ignored: [
-        '**/node_modules/**',
-        '**/.git/**',
-        '**/.svelte-kit/**',
-        '**/data/**',
-        '**/build/**',
-        '**/docker/**',
-        '**/script/**',
-        '**/tests/**',
-      ]
-    }
-  }
+        "**/node_modules/**",
+        "**/.git/**",
+        "**/.svelte-kit/**",
+        "**/data/**",
+        "**/build/**",
+        "**/docker/**",
+        "**/script/**",
+        "**/tests/**",
+      ],
+    },
+  },
 });
